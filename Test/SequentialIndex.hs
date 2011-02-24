1490a5babd0839120c27c27d4d5a6a12ed7201c0
Aristid Breitkreuz <aristidb@googlemail.com>
2011-02-24 03:10:53 +0100

    more convenient test runner

--- Test/SequentialIndex.hs
+++ Test/SequentialIndex.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Test.SequentialIndex
 where
 
 import Data.Maybe
 import Data.SequentialIndex
 import Test.QuickCheck
+import Test.QuickCheck.All
        
 instance Arbitrary SequentialIndex where
     arbitrary = fmap fromJust (fmap tryFromBools arbitrary `suchThat` isJust)
 
 prop_sequentialIndexFromToByteString si = Just si == fromByteString (toByteString si) 
+
+main = $(quickCheckAll)
+
+testWith = $(forAllProperties)
