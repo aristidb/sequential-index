5b6622ee9bce05de21bb33c9cf725b5ccc04ab60
Aristid Breitkreuz <aristidb@googlemail.com>
2011-02-24 04:19:39 +0100

    moar tests

--- Test/SequentialIndex.hs
+++ Test/SequentialIndex.hs
@@ -13,6 +13,24 @@ instance Arbitrary SequentialIndex where
 
 prop_sequentialIndexFromToByteString si = Just si == fromByteString (toByteString si) 
 
+prop_between a b = let m = between a b in (a == m && m == b) || (a < m && m < b) || (b < m && m < a)
+
+prop_leftChild x = case leftChild x of
+                     Nothing -> True
+                     Just v -> v < x
+
+prop_leftChildZero = leftChild zero == Nothing
+prop_leftChildOne = leftChild one == Just root
+prop_leftChildRoot = isJust $ leftChild root
+
+prop_rightChild x = case rightChild x of
+                    Nothing -> True
+                    Just v -> v > x
+
+prop_rightChildZero = rightChild zero == Nothing
+prop_rightChildOne = rightChild one == Nothing
+prop_rightChildRoot = isJust $ rightChild root
+
 main = $(quickCheckAll)
 
 testWith = $(forAllProperties)
