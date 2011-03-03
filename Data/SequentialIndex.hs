c9e4807f461c0015b284984f66457f35621006eb
Aristid Breitkreuz <aristidb@googlemail.com>
2011-03-03 16:28:48 +0100

    more documentation

--- Data/SequentialIndex.hs
+++ Data/SequentialIndex.hs
@@ -64,7 +64,7 @@ zero = SI 0 1
 one :: SequentialIndex
 one = SI 1 1
 
--- | 
+-- | The root of a hypothetical binary tree.
 root :: SequentialIndex
 root = between zero one
 
@@ -74,6 +74,9 @@ commonBase (SI m1 e1) (SI m2 e2) = (m1', m2', e)
           m1' = m1 `shift` (e - e1)
           m2' = m2 `shift` (e - e2)
 
+-- | Construct a 'SequentialIndex' from its 'mantissa' and 'exponent'.
+-- 
+-- Errors are checked and result in a run-time error.
 sequentialIndex :: Integer -> Int -> SequentialIndex
 sequentialIndex 0 _ = zero
 sequentialIndex mx ex
@@ -82,6 +85,9 @@ sequentialIndex mx ex
           | v > one   -> error "Invalid SequentialIndex: beyond one"
           | otherwise -> v
 
+-- | Construct a 'SequentialIndex' from its 'mantissa' and 'exponent'.
+-- 
+-- Errors are checked and result in 'Nothing'.
 trySequentialIndex :: Integer -> Int -> Maybe SequentialIndex
 trySequentialIndex 0 _ = Just zero
 trySequentialIndex mx ex
@@ -90,41 +96,56 @@ trySequentialIndex mx ex
           | v > one   -> Nothing
           | otherwise -> Just v
 
+-- | Construct a 'SequentialIndex' from its 'mantissa' and 'exponent'.
+-- 
+-- Errors are not checked.
 unsafeSequentialIndex :: Integer -> Int -> SequentialIndex
 unsafeSequentialIndex mx ex 
     = until (\(SI m _) -> m `testBit` 0) 
             (\(SI m e) -> SI (m `shiftR` 1) (e - 1)) 
             (SI mx ex)
 
+-- | Construct a 'SequentialIndex' from a list of boolean digits. The exponent
+-- equals the number of digits.
 tryFromBools :: [Bool] -> Maybe SequentialIndex
 tryFromBools = uncurry trySequentialIndex . foldr (\x (s, n) -> ((s `shiftL` 1) .|. (if x then 1 else 0), n + 1)) (0, 0)
 
+-- | Compute a number right in the middle of the arguments.
+-- 
+-- @(x + y) / 2@
 between :: SequentialIndex -> SequentialIndex -> SequentialIndex
 between a b = sequentialIndex (m1 + m2) (e + 1)
     where (m1, m2, e) = commonBase a b
 
+-- | Add digits in front of the mantissa.
 prefixBits :: Int -> Integer -> SequentialIndex -> SequentialIndex
 prefixBits _ _   (SI 0 1) = error "No meaningful prefix for 'zero' possible"
 prefixBits eb mb (SI m e) = sequentialIndex ((mb `shiftL` e) + m) (eb + e + 1)
 
+-- | Build a number from a list of fixed-width mantissa segments.
 build :: Int -> [Integer] -> SequentialIndex
 build nbits xs = foldr (prefixBits nbits) one xs
 
+-- | Build a number from a list of fixed-width mantissa segments.
 buildBits :: (Bits a, Integral a) => [a] -> SequentialIndex
 buildBits xs = build (bitSize $ head xs) (map toInteger xs)
 
+-- | Get the left child of the current path in the hypothetical tree.
 leftChild :: SequentialIndex -> Maybe SequentialIndex
 leftChild (SI 0 1) = Nothing
 leftChild (SI m e) = Just $ SI ((m `shiftR` 1) `shiftL` 2 .|. 1) (e + 1)
 
+-- | Get the right child of the current path in the hypothetical tree.
 rightChild :: SequentialIndex -> Maybe SequentialIndex
 rightChild (SI _ 1) = Nothing
 rightChild (SI m e) = Just $ SI ((m `shiftR` 1) `shiftL` 2 .|. 3) (e + 1)
 
+-- | Get the parent of the current path in the hypothetical tree.
 parent :: SequentialIndex -> Maybe SequentialIndex
 parent (SI _ 1) = Nothing
 parent (SI m e) = Just $ SI ((m `shiftR` 2) `shiftL` 1 .|. 1) (e - 1)
 
+-- | Convert a 'SequentialIndex' to a binary representation.
 toByteString :: SequentialIndex -> B.ByteString
 toByteString (SI m e) = B.unfoldr step (m', e')
     where e' = ((e + 7) `div` 8) * 8
@@ -134,6 +155,7 @@ toByteString (SI m e) = B.unfoldr step (m', e')
           step (v, ex) = let (q, r) = v `divMod` 256
                          in Just (fromInteger r, (q, ex - 8))
 
+-- | Convert a 'SequentialIndex' from its binary representation.
 fromByteString :: B.ByteString -> Maybe SequentialIndex
 fromByteString bs | B.null bs = Just zero
                   | otherwise = trySequentialIndex m e
